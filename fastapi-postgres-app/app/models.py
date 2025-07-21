f1575b018c3117065493b46111c47933d712dd75
pedroclair <pedro.clair@aluno.ufop.edu.br>
2025-07-20 22:06:21 -0300

    remove fasapi folder

--- fastapi-postgres-app/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "user"
-    code = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
